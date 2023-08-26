1ca0dd12ac64ea91af74cf9355751481d611900e
luislong0 <luisptavio.l.p@gmail.com>
2023-08-26 10:07:26 -0300

    first commit zustand

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -31,7 +31,6 @@ export function Player() {
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
-          {/* HEADER */}
           <Header />
 
           <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
